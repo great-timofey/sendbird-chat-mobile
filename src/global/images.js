1a87014f1d256ead3294762e3a56258e3d4ea6c7
Timofei Tverdokhlebov <ttverdokhlebov@allmax.team>
2018-08-22 14:05:52 +0700

    added images

--- src/global/images.js
+++ src/global/images.js
@@ -1,6 +1,8 @@
-
 const images = {
-
+  mail: require('./images/mail/mail.png'),
+  password: require('./images/password/password.png'),
+  eye: require('./images/eye/eye.png'),
+  login: require('./images/login/login.png'),
 };
 
 export default images;
