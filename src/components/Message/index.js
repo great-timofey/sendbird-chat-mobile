b6a776e47d4e58277bafa864a2981688919672c5
Timofei Tverdokhlebov <ttverdokhlebov@allmax.team>
2018-08-29 14:30:39 +0700

    changed Message styling

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -12,21 +12,30 @@ type Props = {
 
 const Message = ({
   message, type, sender, userId, senderId,
-}: Props) => { 
-  const isOwner = userId === senderId; 
+}: Props) => {
+  const isOwner = userId === senderId;
   return (
-  <View
-    style={[
-      styles.container,
-      isOwner ? styles.rightSide : styles.leftSide,
-    ]}
-  >
-      <Text style={[styles.header, isOwner ? styles.owner : styles.notOwner]}>{sender}</Text>
-      <Text style={[styles.message, isOwner ? styles.owner : styles.notOwner]}>
-        {type === 'user' ? message : 'User sent a file message'}
-      </Text>
+    <View style={styles.container}>
+      <View style={styles.dateContainer}>
+        <Text style={styles.dateText}>Date goes here</Text>
+      </View>
+      <View
+        style={[
+          styles.messageContainer,
+          isOwner ? styles.rightSide : styles.leftSide,
+        ]}
+      >
+        <Text style={[styles.header, isOwner ? styles.owner : styles.notOwner]}>
+          {sender}
+        </Text>
+        <Text
+          style={[styles.message, isOwner ? styles.owner : styles.notOwner]}
+        >
+          {type === 'user' ? message : 'User sent a file message'}
+        </Text>
+      </View>
     </View>
-  )
-  };
+  );
+};
 
 export default Message;
--- src/components/Message/styles.js
+++ src/components/Message/styles.js
@@ -3,23 +3,25 @@ import colors from '../../global/colors';
 
 export default StyleSheet.create({
   container: {
+    marginVertical: 20,
+  },
+  messageContainer: {
+    padding: 10,
     minWidth: 150,
     minHeight: 80,
-    padding: 10,
-    marginVertical: 5,
-    borderRadius: 5,
-    backgroundColor: colors.blue,
+    borderRadius: 10,
   },
   leftSide: {
-    alignSelf: 'flex-start',
-    backgroundColor: colors.white,
+    backgroundColor: colors.darkGreyTransparent,
     borderWidth: 1,
-    borderColor: colors.white20,
+    borderColor: colors.white30,
     color: colors.black,
+    alignSelf: 'flex-start',
   },
   rightSide: {
-    alignSelf: 'flex-end',
+    backgroundColor: colors.blue,
     color: colors.white,
+    alignSelf: 'flex-end',
   },
   header: {
     textAlign: 'center',
@@ -34,5 +36,20 @@ export default StyleSheet.create({
   },
   notOwner: {
     color: colors.black,
+  },
+  dateContainer: {
+    width: 75,
+    height: 20,
+    top: -25,
+    alignSelf: 'center',
+    position: 'absolute',
+    backgroundColor: colors.darkSkyBlue,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  dateText: {
+    fontSize: 9,
+    color: colors.darkGreyBlueTwo,
   },
 });
