8f23f911f15342ef25195544892cbffe73530338
Timofei Tverdokhlebov <ttverdokhlebov@allmax.team>
2018-08-29 11:42:23 +0700

    added selector for getting current online message

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,2 +1,3 @@
 export const currentChannelSelector = state => state.user.currentChannel;
 export const currentUserSelector = state => state.user.user;
+export const currentOnlineMessageSelector = state => state.common.currentOnlineMessage;
